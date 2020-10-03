d421a81d278ae61bb53fdffb81bc8e4a8486a4d9
Greg Rodriguez <greg_orio15323@yahoo.com>
2020-10-03 00:16:57 -0600

    java update questions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charatersNum = 0;
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", ".", "?"];
+var Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +14,21 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function getInfo() {
+
+    var getPasswordLength = parseInt(prompt("How long would you like your password to be? 8 - 128 Characters max"));
+    if (isNaN(getPasswordLength)) {
+        alert("Try Again. Enter a number.");
+        getInfo();
+    } else if (getPasswordLength < 8 || getPasswordLength > 128) {
+        alert("Try Again. Enter number between 8-128.");
+        getInfo();
+    }
+    var askSpecialCharacter = confirm("Do you want special characters in your Password?");
+    console.log(getPasswordLength);
+}
+getInfo(); 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
